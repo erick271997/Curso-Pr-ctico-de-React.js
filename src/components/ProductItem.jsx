335557d5254c04d89f0515a8254472662b0ce757
Erick <erickcampaz@gmail.com>
2022-10-01 08:57:42 -0500

    Dandole vida a un bunttom con React.useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import '../styles/ProductItem.scss';
+//Para generar evento al botton de Pro que de carrtito verde
+
+import React,{useState} from 'react';
+import '@styles/ProductItem.scss';
+import addTocart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+	const [cart, setCart] = useState('');
+
+	const handleClick = () =>{
+		setCart('hola mundo');
+	}
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -10,12 +18,14 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick={handleClick} > 
+					<img src={addTocart} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
 }
 
 export default ProductItem;
+//se pone en etiqueta figure ya que ahi se le va a dar click
