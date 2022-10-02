90b8da96b5306292e3a839efb581d852aac99c83
Erick <erickcampaz@gmail.com>
2022-10-01 21:04:20 -0500

    Creado intalando axion para que apareciera varios elemento de la tienda

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axion from 'axios'; // esta se tiene que instalar para que aparezca todo lo item es decir todos los productos
+
+const API = 'http://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(async() => {
+		const response = await axion (API);
+		setProducts(response.data);
+
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(products =>(
+					<ProductItem />
+				))}
+				
 			</div>
 		</section>
 	);
